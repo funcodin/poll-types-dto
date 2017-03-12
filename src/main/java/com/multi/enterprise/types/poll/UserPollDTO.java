db86a57710502e2b83ad94d401dd4b167ea4f8dd
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-03-12 01:24:59 -0800

    added dto for user polls

---
+++ src/main/java/com/multi/enterprise/types/poll/UserPollDTO.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.multi.enterprise.types.poll;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author Robot
+ *
+ */
+@XmlType
+@XmlRootElement(name = "userPoll")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class UserPollDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// This need to return total count of answers
+	// count of each options;
+
+	private String userId;
+	private String questionId;
+	private String optionId;
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the questionId
+	 */
+	public String getQuestionId() {
+		return questionId;
+	}
+
+	/**
+	 * @param questionId the questionId to set
+	 */
+	public void setQuestionId(String questionId) {
+		this.questionId = questionId;
+	}
+
+	/**
+	 * @return the optionId
+	 */
+	public String getOptionId() {
+		return optionId;
+	}
+
+	/**
+	 * @param optionId the optionId to set
+	 */
+	public void setOptionId(String optionId) {
+		this.optionId = optionId;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
