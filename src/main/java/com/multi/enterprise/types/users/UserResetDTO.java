c79adc8c6cbd60cc3fe2930d09f73d1a48e39f9f
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-04-23 00:23:59 -0700

    Added user reset dto

---
+++ src/main/java/com/multi/enterprise/types/users/UserResetDTO.java
@@ -0,0 +1,71 @@
+package com.multi.enterprise.types.users;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * 
+ */
+
+/**
+ * @author Robot
+ *
+ */
+
+@XmlType
+@XmlRootElement(name = "userReset")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class UserResetDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String email;
+	private String responseMessage;
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the responseMessage
+	 */
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+	/**
+	 * @param responseMessage the responseMessage to set
+	 */
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+}
