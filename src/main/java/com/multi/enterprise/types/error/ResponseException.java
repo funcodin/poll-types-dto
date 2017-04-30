0aeecba4e7e1cfa91e7f87c5ab34fc7b17e86ebe
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-04-30 16:39:11 -0700

    Added error type

---
+++ src/main/java/com/multi/enterprise/types/error/ResponseException.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.multi.enterprise.types.error;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author Robot
+ *
+ */
+
+@XmlType
+@XmlRootElement(name = "ResponseError")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ResponseException {
+
+	private String errorMessage;
+	private int statusCode;
+
+	/**
+	 * @return the errorMessage
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	/**
+	 * @param errorMessage the errorMessage to set
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	/**
+	 * @return the statusCode
+	 */
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	/**
+	 * @param statusCode the statusCode to set
+	 */
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+}
